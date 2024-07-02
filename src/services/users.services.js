80424ea12bf1fc2049cf18c8009cc2a2ddff876d
Shibun singh deo <shibakumarsinghdeo@gmail.com>
2024-07-02 09:11:38 +0530

    Done

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -16,11 +16,10 @@ export const saveDataToDatabase = async (data) => {
 
 
 export const loginFromDatabase = async (data) => {
-    let email = data.email;
-    let password = data.password;
+    const email = data.email;
+    const password = data.password;
     const user = await userDataModel.findOne({ email: email });
-    const user_id = user.id;
-    
+
     if (!user) {
         console.log("User not found");
         throw new ApiError(401, "Invalid email or password");
@@ -33,5 +32,8 @@ export const loginFromDatabase = async (data) => {
     }
 
     const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    return { user, token };
-}
+
+    const { password: userPassword, ...userWithoutPassword } = user.toObject();
+
+    return { user: userWithoutPassword, token };
+};
